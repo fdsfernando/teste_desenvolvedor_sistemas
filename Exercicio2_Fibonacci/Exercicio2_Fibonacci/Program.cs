eef24286a93999de822fcb061e9a8eca2e71ad10
fdsfernando <91353772+fdsfernando@users.noreply.github.com>
2021-09-26 22:20:53 -0300

    Add files via upload

---
+++ Exercicio2_Fibonacci/Exercicio2_Fibonacci/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Exercicio2_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracao das Variaveis
+            int valor;
+            int numAnterior, numAtual = 0;            
+            int numProximo = 1;
+            int indice;
+            bool numEncontrado = false;
+
+            //Informa ao usuario o proposito do programa
+            Console.WriteLine("..........Sequência Fibonacci..........");
+            Console.WriteLine("Digite um numero para verificar se existe na Sequência Fibonacci:");
+            valor = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            //Laco de repeticao para calcular os numeros da sequência Fibonacci
+            for (indice = 0; indice <= valor; indice++)
+            {
+                //Mostra na tela o numero atual da sequencia
+                Console.WriteLine("Numero Sequência Fibonacci: {0}", numAtual);
+
+                //verifica se o numero digitado pertence a sequencia
+                if(valor == numAtual)
+                {
+                    numEncontrado = true;
+                }
+
+                //Manipulacao das variaveis para calcular o proximo numero da sequencia
+                numAnterior = numAtual;
+                numAtual = numProximo;
+                numProximo = numAnterior + numAtual;                
+            }
+
+            Console.WriteLine("");
+            
+            //Informa se o numero digitado existe ou nao na sequencia para o usuario
+            if(numEncontrado == false)
+            {
+                Console.WriteLine("Número não encontrado na Sequência Fibonacci!");
+            }
+            else
+            {
+                Console.WriteLine("Número encontrado na Sequência Fibonacci!");
+            }
+        }
+    }
+}
